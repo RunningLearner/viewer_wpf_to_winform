a6f13fe16b06e9229db6346d81649de4f1015710
RunningLearner <97277365+RunningLearner@users.noreply.github.com>
2025-07-02 17:08:53 +0900

    Add Entities project and references

---
+++ dicom_viewer_winform/Entities/Spacing3.cs
@@ -0,0 +1,14 @@
+﻿namespace Entities
+{
+    public class Spacing3
+    {
+        public double X { get; set; } = 1;
+        public double Y { get; set; } = 1;
+        public double Z { get; set; } = 1;
+
+        public Vector3 AsVector()
+        {
+            return new Vector3(X, Y, Z);
+        }
+    }
+}
