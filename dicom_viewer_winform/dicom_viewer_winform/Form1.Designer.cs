a2cc0bcc5cdb8e80b77a8fee50fbdfdd8545ab54
RunningLearner <97277365+RunningLearner@users.noreply.github.com>
2025-07-02 10:46:23 +0900

    Show selected DICOM image

--- dicom_viewer_winform/dicom_viewer_winform/Form1.Designer.cs
+++ dicom_viewer_winform/dicom_viewer_winform/Form1.Designer.cs
@@ -26,12 +26,63 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(12, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(94, 29);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open Folder";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // pictureBox1
+            //
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+                        | System.Windows.Forms.AnchorStyles.Left) 
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 391);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPath.Location = new System.Drawing.Point(112, 14);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.ReadOnly = true;
+            this.textBoxPath.Size = new System.Drawing.Size(676, 27);
+            this.textBoxPath.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.buttonOpen);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
--- dicom_viewer_winform/dicom_viewer_winform/Form1.cs
+++ dicom_viewer_winform/dicom_viewer_winform/Form1.cs
@@ -1,3 +1,6 @@
+using Dicom.Imaging;
+using System.Linq;
+
 namespace dicom_viewer_winform
 {
     public partial class Form1 : Form
@@ -5,6 +8,30 @@ namespace dicom_viewer_winform
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            using var dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPath.Text = dialog.SelectedPath;
+
+                var loader = new DataSetSelector();
+                loader.Open(dialog.SelectedPath);
+
+                var firstSeries = loader.Series?.FirstOrDefault() as DicomViewer.IO.DicomSeries;
+                if (firstSeries != null && firstSeries.FileNames.Count > 0)
+                {
+                    var middleFile = firstSeries.FileNames[firstSeries.FileNames.Count / 2];
+                    var dicomImage = new Dicom.Imaging.DicomImage(middleFile);
+                    var frame = dicomImage.NumberOfFrames > 1 ? dicomImage.NumberOfFrames / 2 : 0;
+                    using var rendered = dicomImage.RenderImage(frame);
+                    var bitmap = rendered.AsClonedBitmap();
+                    pictureBox1.Image?.Dispose();
+                    pictureBox1.Image = bitmap;
+                }
+            }
         }
     }
 }
